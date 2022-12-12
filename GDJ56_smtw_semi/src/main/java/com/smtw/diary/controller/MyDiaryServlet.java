eaa9466199b7d040d27437ae8c224b4e2c60c4bb
Jihee <mgh6958@naver.com>
2022-12-12 17:20:52 +0900

    나의 출국일지 DB를 가지고 있지 않는 경우 - 값 받아서 저장

--- GDJ56_smtw_semi/src/main/java/com/smtw/diary/controller/MyDiaryServlet.java
+++ GDJ56_smtw_semi/src/main/java/com/smtw/diary/controller/MyDiaryServlet.java
@@ -1,11 +1,15 @@
 package com.smtw.diary.controller;
 
 import java.io.IOException;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.smtw.diary.model.service.DiaryService;
 
 /**
  * Servlet implementation class MyDiaryServlet
@@ -26,7 +30,28 @@ public class MyDiaryServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/views/diary/myDiary.jsp").forward(request, response);
+		String memberId=request.getParameter("memberId");
+		String leavingdate=request.getParameter("leavingdate");
+		String alarmcheck=request.getParameter("alarmcheck");
+		
+		System.out.println(alarmcheck);
+
+		int result=new DiaryService().insertDiary(memberId,leavingdate,alarmcheck);
+		System.out.println(result);
+		
+		String msg="", loc="";
+		
+		if(result>0) {
+			msg="나의 출국일지 저장 성공";
+			loc="/diary/firstDiary.do";
+		}else {
+			msg="나의 출국일지 저장 실패";
+			loc="/diary/firstDiary.do";
+		}
+		request.setAttribute("msg", msg);
+		request.setAttribute("loc", loc);
+		
+		request.getRequestDispatcher("/views/common/msg.jsp").forward(request, response);
 	}
 
 	/**
