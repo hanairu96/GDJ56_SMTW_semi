fdd3242dbd0ee60a7bca9a8ea1ea3b9de9cc89e9
ellie <ellie@DESKTOP-RI4I0IG>
2022-12-12 16:25:40 +0900

    yeji - Country vo 패키지에 Country, CountryPage, CountryInfo, NationOfLike 4개의 클래스를 생성했습니다.

---
+++ GDJ56_smtw_semi/src/main/java/com/smtw/country/model/vo/Country.java
@@ -0,0 +1,22 @@
+package com.smtw.country.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Country {
+	private String nName;
+	private String nLanguage;
+	private String nTend;
+	private String nPlace;
+	private String nSeason;
+	private String nImg;
+	private String npharse;
+	
+	
+}
