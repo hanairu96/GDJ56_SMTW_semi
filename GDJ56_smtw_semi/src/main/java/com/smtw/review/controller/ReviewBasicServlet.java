029a8320a732030af4bc6d434f4540180822d63b
DevJangSunWoo <sw6yyrdn@gmail.com>
2022-12-12 16:50:11 +0900

    20221212 REVIEW 클래스 review,reviewbasicservlet, reviewcommnets,review dao, reviewservice 생성 reviewsql.propertiest 생성 reviewbasic.jsp 생성

---
+++ GDJ56_smtw_semi/src/main/java/com/smtw/review/controller/ReviewBasicServlet.java
@@ -0,0 +1,79 @@
+package com.smtw.review.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.smtw.review.model.service.ReviewService;
+import com.smtw.review.model.vo.Review;
+
+/**
+ * Servlet implementation class ReviewBasicServlet
+ */
+@WebServlet("/community/reviewBasic.do")
+public class ReviewBasicServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReviewBasicServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		//페이징 바처리하기11
+		
+		int cPage;
+		int numPerpage=8;
+		try {
+			cPage=Integer.parseInt(request.getParameter("cPage"));
+		}catch(NumberFormatException e) {
+			//null 을 파싱할수 없으니
+			cPage=1;
+			
+			
+		}
+		
+		List<Review> list=new ReviewService().searchReviewAll(cPage,numPerpage);
+		
+		
+		
+		int totalData=new ReviewService().selectReviewCount();
+		
+		
+		// 코드를 생성할수 있는 변숫 선언
+		String pageBar="";
+		int pageBarSize=4;
+		int totalPage=(int)Math.ceil((double)totalData/numPerpage);
+		int pageNo=((cPage-1)/pageBarSize)*pageBarSize+1;
+		int pageEnd=pageNo+pageBarSize-1;
+		
+		
+		
+		
+		
+		request.getRequestDispatcher("/views/community/reviewBasic.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
