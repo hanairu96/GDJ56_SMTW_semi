738abb7a9392384cf91d3ed629fb97a8eb8427ca
Jihee <mgh6958@naver.com>
2022-12-13 20:32:30 +0900

    체크리스트찾기, 체크리스트등록, 체크리스트수정 기능 추가

--- GDJ56_smtw_semi/src/main/java/com/smtw/diary/model/service/DiaryService.java
+++ GDJ56_smtw_semi/src/main/java/com/smtw/diary/model/service/DiaryService.java
@@ -8,6 +8,7 @@ import static com.smtw.common.JDBCTemplate.getConnection;
 import java.sql.Connection;
 
 import com.smtw.diary.model.dao.DiaryDao;
+import com.smtw.diary.model.vo.CheckList;
 import com.smtw.diary.model.vo.Diary;
 
 public class DiaryService {
@@ -38,6 +39,38 @@ public class DiaryService {
 		Connection conn=getConnection();
 		int result=0;
 		result=dao.updateDiary(conn,memberId, leavingdate, alarmYN);
+		
+		if(result>0) commit(conn);
+		else rollback(conn);
+		
+		close(conn);
+		return result;
+	}
+	
+	public CheckList searchCheckList(String memberId) {
+		Connection conn=getConnection();
+		CheckList checkList=dao.searchCheckList(conn,memberId);
+		
+		close(conn);
+		return checkList;	
+	}
+	
+	public int insertCheckList(CheckList check) {
+		Connection conn=getConnection();
+		int result=0;
+		result=dao.insertCheckList(conn,check);
+		
+		if(result>0) commit(conn);
+		else rollback(conn);
+		
+		close(conn);
+		return result;
+	}
+	
+	public int updateCheckList(CheckList check) {
+		Connection conn=getConnection();
+		int result=0;
+		result=dao.updateCheckList(conn,check);
 		
 		if(result>0) commit(conn);
 		else rollback(conn);
