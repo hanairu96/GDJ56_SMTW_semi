c458aff35d1cac471474418d85f7bd7cc20440c6
Jihee <mgh6958@naver.com>
2022-12-14 20:02:11 +0900

    사용자가 선택한 값에 따라 나에게 맞는 나라 결과 출력 서블릿

--- GDJ56_smtw_semi/src/main/java/com/smtw/mycountry/controller/MyCountryResultServlet.java
+++ GDJ56_smtw_semi/src/main/java/com/smtw/mycountry/controller/MyCountryResultServlet.java
@@ -1,11 +1,17 @@
 package com.smtw.mycountry.controller;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.smtw.mycountry.model.service.MyCountryService;
+import com.smtw.mycountry.model.vo.MyCountry;
 
 /**
  * Servlet implementation class MyCountryResultServlet
@@ -26,6 +32,19 @@ public class MyCountryResultServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String myCountryResult=request.getParameter("result");
+		String[] countryResult=myCountryResult.split(",");
+		System.out.println(Arrays.toString(countryResult));
+		
+		String nLanguage=countryResult[0];
+		String nPlace=countryResult[1];
+		String nSeason=countryResult[2];
+		String nTend=countryResult[3];
+		
+		List<MyCountry> mycountry=new MyCountryService().searchMyCountry(nLanguage,nPlace,nSeason,nTend);
+		
+		request.setAttribute("countryResult", countryResult);
+		request.setAttribute("mycountry", mycountry);
 		request.getRequestDispatcher("/views/mycountry/myCountryResult.jsp").forward(request, response);
 	}
 
