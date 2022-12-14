81a7738a0df59f9b9457e23e1f78dc43d8f0f252
Nari <hnr616@gmail.com>
2022-12-14 17:34:55 +0900

    공지사항 리스트 페이징처리-NR

--- GDJ56_smtw_semi/src/main/java/com/smtw/notice/controller/NoticeListServlet.java
+++ GDJ56_smtw_semi/src/main/java/com/smtw/notice/controller/NoticeListServlet.java
@@ -1,11 +1,16 @@
 package com.smtw.notice.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.smtw.notice.model.service.NoticeService;
+import com.smtw.notice.model.vo.Notice;
 
 /**
  * Servlet implementation class noticeListServlet
@@ -26,6 +31,52 @@ public class NoticeListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//공지사항 메인메뉴
+		int cPage;
+		int numPerpage;
+		try {
+			cPage=Integer.parseInt(request.getParameter("cPage"));
+		}catch(NumberFormatException e) {
+			cPage=1;
+		}
+		numPerpage=5;
+		String pageBar="";
+		
+		//현재 있는 공지사항 리스트들
+		List<Notice> list=new NoticeService().selectNoticeList(cPage,numPerpage);
+		//전체 공지사항 개수
+		int totalData=new NoticeService().selectNoticeCount();
+		
+		int totalPage=(int)Math.ceil((double)totalData/numPerpage);//총 페이지 수= 전체공지사항개수/5
+		
+		int pageBarsize=5;
+		
+		int pageNo=((cPage-1)/pageBarsize)*pageBarsize+1;
+		int pageEnd=pageNo+pageBarsize-1;
+		
+		if(pageNo==1) {
+			pageBar+="<a>&laquo;</a>";
+		}else {
+			pageBar+="<a href='"+request.getRequestURL()+"?cPage="+(pageNo-1)+"'>&laquo;</a>";
+		}
+		while(!(pageNo>pageEnd||pageNo>totalPage)) {
+			if(cPage==pageNo) {
+				//보고있는 페이지
+				pageBar+="<span>"+pageNo+"</span>";
+			}else {
+				pageBar+="<a href='"+request.getRequestURL()+"?cPage="+pageNo+"'>"+pageNo+"</a>";
+			}
+			pageNo++;
+		}
+		if(pageNo>totalPage) {
+			pageBar+="<a>&raquo;</a>";
+		}else {
+			pageBar+="<a href='"+request.getRequestURL()+"cPage="+pageNo+"'>&raquo;</a>";
+		}
+		
+		request.setAttribute("notices", list);
+		request.setAttribute("pageBar", pageBar);
+		
 		request.getRequestDispatcher("/views/notice/noticeList.jsp").forward(request, response);
 	}
 
