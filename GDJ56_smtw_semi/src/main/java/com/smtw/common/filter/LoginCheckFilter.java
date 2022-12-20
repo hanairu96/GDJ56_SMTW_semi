394afd521400db074731fbb465e629878f7123e5
dllo0 <dllo0@DESKTOP-HGKV66H>
2022-12-20 16:00:10 +0900

    로그인 체크 필터 변경

--- GDJ56_smtw_semi/src/main/java/com/smtw/common/filter/LoginCheckFilter.java
+++ GDJ56_smtw_semi/src/main/java/com/smtw/common/filter/LoginCheckFilter.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import com.smtw.common.exception.LoginException;
 import com.smtw.member.model.vo.Member;
 
 /**
@@ -51,9 +52,10 @@ public class LoginCheckFilter extends HttpFilter implements Filter {
 	    	  chain.doFilter(request, response); // 화면 보여줌
 	      }
 	      else {
-	    	  request.setAttribute("msg", "로그인 시 이용할 수 있는 서비스 입니다 :(");
-	    	  request.setAttribute("loc", "/");
-	    	  request.getRequestDispatcher("/views/common/msg.jsp").forward(request, response);
+	    	  throw new LoginException("로그인 시 이용할 수 있는 서비스");
+//	    	  request.setAttribute("msg", "로그인 시 이용할 수 있는 서비스 입니다 :(");
+//	    	  request.setAttribute("loc", "/");
+//	    	  request.getRequestDispatcher("/views/common/msg.jsp").forward(request, response);
 	      }
 	}
 			
